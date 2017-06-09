c8fc1b14dd394afbeef1429976c2b81ad5d9517c
Chris Sobeck <csobeck@med.umich.edu>
2017-06-09 10:58:15 -0400

    Make composer project with working example

--- test/parse.php
+++ test/parse.php
@@ -1,2 +1,9 @@
-<?php 
+<?php
 
+require(dirname(__FILE__) . '/../vendor/autoload.php');
+
+$file = '~/path/to/file';
+$docx = new DocX2HtmlParser();
+$docx->setFile($file);
+$html = $docx->toHtml();
+echo $html;
